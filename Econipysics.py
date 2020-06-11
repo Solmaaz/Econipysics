1b1c257f6e947c5a50d1c05fa0614e517cf48fc0
Solmaz <Solmaz.gl@gmail.com>
2020-06-12 00:42:12 +0430

    Statistical properties has been added

--- Econipysics.py
+++ Econipysics.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import pandas_datareader as pdr 
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 aapl =pdr.get_data_yahoo('AAPL', '2019-05-04')
 aapl.head()
@@ -13,3 +14,17 @@ EMA =df.ewm(com=0.5).mean()
 print(EMA.head())
 EMA.to_csv(r'ema.txt', header=None, index=True, sep=' ')
 df.to_csv(r'close-prices.txt', header=None, index=True, sep=' ')
+sns.distplot(df)
+plt.show()
+Mean = df.mean()
+print("Mean value = ")
+print(Mean)
+Std = df.std()
+print("Standard deviation = ")
+print(Std)
+skewnes = df.skew()
+print("Skewnes = ")
+print(skewnes)
+kurtosis = df.kurtosis()
+print("Kurtosis = ")
+print(kurtosis)
